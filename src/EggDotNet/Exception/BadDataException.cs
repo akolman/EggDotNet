793d8db15fc68f179285bfeae3fb4e1add7dd81a
Andrew Kolman <Andrew.Kolman@hyland.com>
2023-11-20 23:26:30 -0600

    More

--- src/EggDotNet/Exception/BadDataException.cs
+++ src/EggDotNet/Exception/BadDataException.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EggDotNet.Exception
+﻿namespace EggDotNet.Exception
 {
 	internal class BadDataException : Eggception
 	{
