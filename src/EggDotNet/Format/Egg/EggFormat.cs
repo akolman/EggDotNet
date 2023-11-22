129b3eb68c3572d545fa2ce0eaf0703a2466acd9
Andrew Kolman <Andrew.Kolman@hyland.com>
2023-11-21 23:07:05 -0600

    Cleanup

--- src/EggDotNet/Format/Egg/EggFormat.cs
+++ src/EggDotNet/Format/Egg/EggFormat.cs
@@ -6,16 +6,16 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace EggDotNet.Format.Egg
 {
 #pragma warning disable CA1852
-
 	internal class EggFormat : IEggFileFormat
 	{
 		private readonly Func<Stream, IEnumerable<Stream>>? _streamCallback;
 		private readonly Func<string>? _pwCallback;
-		private List<EggVolume>? _volumes;
+		private readonly List<EggVolume> _volumes = new(8);
 		private List<EggEntry>? _entriesCache;
 		private bool disposedValue;
 
@@ -28,62 +28,18 @@ namespace EggDotNet.Format.Egg
 		public void ParseHeaders(Stream stream, bool ownStream)
 		{
 			var initialVolume = EggVolume.Parse(stream, ownStream);
-			_volumes = new List<EggVolume>() { initialVolume };
+			_volumes.Add(initialVolume);
 
 			if (initialVolume.IsSplit)
 			{
-				if (_streamCallback == null)
-				{
-					throw new InvalidOperationException("Stream callback not set");
-				}
-
-				var streams = _streamCallback.Invoke(stream);
-
-				foreach (var extStream in streams)
-				{
-					extStream.Seek(4, SeekOrigin.Begin);
-					var extVolume = EggVolume.Parse(extStream, true);
-					if (extVolume.Header.HeaderId != initialVolume.Header.HeaderId)
-					{
-						_volumes.Add(extVolume);
-					}
-				}
+				FetchAndParseSplitVolumes(stream);
 			}
-			
-		}
-
-		private Stream PrepareStream()
-		{
-			Stream? st = null;
-			if (_volumes != null && _volumes.Count > 1)
-			{
-				var subStreams = new List<SubStream>(_volumes.Count);
-				var curVol = _volumes.Single(v => v.Header.SplitHeader!.PreviousFileId == 0);
-				var curSt = curVol.GetStream();
-				//subStreams.Add(new SubStream(curSt, curSt.Position));
-				subStreams.Add(new SubStream(curSt, curVol.Header.HeaderEndPosition));
-
-				while (curVol.Header.SplitHeader!.NextFileId != 0)
-				{
-					curVol = _volumes.Single(v => v.Header.HeaderId == curVol.Header.SplitHeader.NextFileId);
-					curSt = curVol.GetStream();
-					subStreams.Add(new SubStream(curSt, curVol.Header.HeaderEndPosition));
-				}
-
-				st = new CollectiveStream(subStreams);
-			}
-			else
-			{
-				st = new FakeDisposingStream(_volumes.Single().GetStream());
-			}
-
-			return st;
 		}
 
 		public List<EggArchiveEntry> Scan(EggArchive archive)
 		{
 			using var st = PrepareStream();
-			
+
 			_entriesCache = EggEntry.ParseEntries(st, archive);
 
 			var ret = new List<EggArchiveEntry>();
@@ -106,6 +62,99 @@ namespace EggDotNet.Format.Egg
 			return ret;
 		}
 
+		private void FetchAndParseSplitVolumes(Stream stream)
+		{
+			if (_streamCallback == null)
+			{
+				throw new InvalidOperationException("Stream callback not set");
+			}
+
+			var initialVolume = _volumes.Single();
+
+			var streams = _streamCallback.Invoke(stream);
+
+			foreach (var extStream in streams)
+			{
+				extStream.Seek(4, SeekOrigin.Begin);
+				var extVolume = EggVolume.Parse(extStream, true);
+				if (extVolume.Header.HeaderId != initialVolume.Header.HeaderId
+					&& !_volumes.Any(v => v.Header.HeaderId == extVolume.Header.HeaderId))
+				{
+					_volumes!.Add(extVolume);
+				}
+			}
+		}
+
+		private Stream PrepareStream()
+		{
+			if (_volumes.Count == 0) throw new Eggception("Volume collection empty");
+
+			if (_volumes.Count == 1)
+			{
+				return new FakeDisposingStream(_volumes.Single().GetStream());
+			}
+			else
+			{
+				return PrepareSplitStream();
+			}
+		}
+
+		private CollectiveStream PrepareSplitStream()
+		{
+			var subStreams = new List<SubStream>(_volumes.Count);
+			var curVol = _volumes.Single(v => v.Header.SplitHeader!.PreviousFileId == 0);
+			var curSt = curVol.GetStream();
+			subStreams.Add(new SubStream(curSt, curVol.Header.HeaderEndPosition));
+
+			while (curVol.Header.SplitHeader!.NextFileId != 0)
+			{
+				curVol = _volumes.Single(v => v.Header.HeaderId == curVol.Header.SplitHeader.NextFileId);
+				curSt = curVol.GetStream();
+				subStreams.Add(new SubStream(curSt, curVol.Header.HeaderEndPosition));
+			}
+
+			return new CollectiveStream(subStreams);
+		}
+
+		public Stream GetStreamForEntry(EggArchiveEntry entry)
+		{
+			var st = PrepareStream();
+			Stream subSt = new SubStream(st, entry.PositionInStream, entry.PositionInStream + entry.CompressedLength);
+			var eggEntry = _entriesCache.Single(e => e.Id == entry.Id);
+
+			if (eggEntry.EncryptHeader != null)
+			{
+				subSt = GetDecryptionStream(subSt, eggEntry);
+			}
+
+			return GetDecompressionStream(subSt, eggEntry);
+		}
+
+		private Stream GetDecryptionStream(Stream subSt, EggEntry eggEntry)
+		{
+			var pwCb = _pwCallback ?? DefaultStreamCallbacks.GetPasswordCallback();
+
+			while (true)
+			{
+				var pw = pwCb.Invoke();
+				IStreamDecryptionProvider? s;
+				if (eggEntry.EncryptHeader!.EncryptionMethod == EncryptionMethod.Standard)
+				{
+					s = new ZipStreamDecryptionProvider(eggEntry.EncryptHeader.Param1, eggEntry.EncryptHeader.Param2, pw);
+				}
+				else
+				{
+					var width = eggEntry.EncryptHeader.EncryptionMethod == EncryptionMethod.AES256 ? 256 : 128;
+					s = new AesStreamDecryptionProvider(width, eggEntry.EncryptHeader.Param1, eggEntry.EncryptHeader.Param2, pw);
+				}
+
+				if (s.PasswordValid)
+				{
+					return s.GetDecryptionStream(subSt);
+				}
+			}
+		}
+
 		private static Stream GetDecompressionStream(Stream stream, EggEntry entry)
 		{
 			IStreamCompressionProvider? compressor;
@@ -119,44 +168,6 @@ namespace EggDotNet.Format.Egg
 				_ => throw new UnknownCompressionEggception(),
 			};
 			return compressor.GetDecompressStream(stream);
-		}
-
-		public Stream GetStreamForEntry(EggArchiveEntry entry)
-		{
-			var st = PrepareStream();
-			Stream subSt = new SubStream(st, entry.PositionInStream, entry.PositionInStream + entry.CompressedLength);
-			var eggEntry = _entriesCache.Single(e => e.Id == entry.Id);
-
-			if (eggEntry.EncryptHeader != null)
-			{
-				var pwCb = _pwCallback ?? DefaultStreamCallbacks.GetPasswordCallback();
-
-				while (true)
-				{
-					var pw = pwCb.Invoke();
-					if (eggEntry.EncryptHeader.EncryptionMethod == EncryptionMethod.Standard)
-					{
-						var s = new ZipStreamDecryptionProvider(eggEntry.EncryptHeader.Param1, eggEntry.EncryptHeader.Param2, pw);
-						if (s.PasswordValid)
-						{
-							subSt = s.GetDecryptionStream(subSt);
-							break;
-						}
-					}
-					else
-					{
-						var width = eggEntry.EncryptHeader.EncryptionMethod == EncryptionMethod.AES256 ? 256 : 128;
-						var s = new AesStreamDecryptionProvider(width, eggEntry.EncryptHeader.Param1, eggEntry.EncryptHeader.Param2, pw);
-						if (s.PasswordValid)
-						{
-							subSt = s.GetDecryptionStream(subSt);
-							break;
-						}
-					}
-				}
-			}
-
-			return GetDecompressionStream(subSt, eggEntry);
 		}
 
 		protected virtual void Dispose(bool disposing)
