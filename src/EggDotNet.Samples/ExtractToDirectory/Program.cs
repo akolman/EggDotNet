ec58a633e42de9855784fb6f89f83e9dcc02472f
Andrew Kolman <Andrew.Kolman@hyland.com>
2026-02-15 09:55:56 -0600

    Added samples path and project.  More to be added later.

---
+++ src/EggDotNet.Samples/ExtractToDirectory/Program.cs
@@ -0,0 +1,43 @@
+﻿using EggDotNet;
+using System.Diagnostics;
+
+namespace EggDotNet.Samples.ExtractToDirectory
+{
+	internal class Program
+	{
+		internal class ProgressReporter
+		{
+			private Stopwatch sw = new Stopwatch();
+
+			internal void ReportStart(EggArchiveEntry entry, long written, long total)
+			{
+				sw.Restart();
+				Console.WriteLine($"Starting extract of {entry.FullName} ({entry.UncompressedLength} bytes)");
+			}
+
+			internal void ReportEnd(EggArchiveEntry entry, long written, long total)
+			{
+				sw.Stop();
+				double pct = (double)written * 100 / total;
+				Console.WriteLine($"Finished extract of {entry.FullName} in {sw.ElapsedMilliseconds} ms : {pct.ToString("F2")}% complete");
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			var sampleRootPath = @"C:\Users\akolman\source\repos\EggDotNet\src\EggDotNet.Samples\SampleFiles";
+			var archiveName = "defaults.egg";
+			var archivePath = Path.Combine(sampleRootPath, archiveName);
+			var outputPath = @"../../../output";
+			var reporter = new ProgressReporter();
+			try
+			{
+				EggFile.ExtractToDirectory(archivePath, outputPath, reporter.ReportStart, reporter.ReportEnd);
+			}
+			catch(Exception e) 
+			{
+				Console.Error.WriteLine($"Encountered exception extracting archive {archiveName}: {e.ToString()}");
+			}
+		}
+	}
+}
